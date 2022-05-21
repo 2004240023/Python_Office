17cac06d914562ccbe072af0ee3facd9ef8a3d7e
yamamotoryuo <2004240023yr@edu.oca.ac.jp>
2022-05-21 16:36:56 +0900

    commit

--- test_excel.py
+++ test_excel.py
@@ -14,5 +14,32 @@ ws.append([1, 2, 3])
 import datetime
 ws['A2'] = datetime.datetime.now()
 
+#--追加
+
+#for x in range(1,101):
+#    for y in range(1,101):
+#        ws.cell(row=x,column=y)
+
+a = 'A'
+b = 'B'
+#for x in range(1,101):
+#    for y in range(1,101):
+#        d = a + str(x)
+#        D = b + str(x)
+#        ws[d] = x
+#        ws[D] = x
+
+for x in range(1,11):
+    for y in range(1,11):
+        ws.cell(row=x,column=y)
+
+
+#d = 10
+#c = a + str(d)
+#ws[c] = 10
+
+
+
+
 # Save the file
 wb.save("sample.xlsx")
