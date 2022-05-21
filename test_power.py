84ec9ef23d8bad20a0199e60fe6f649e591ec067
yamamotoryuo <2004240023yr@edu.oca.ac.jp>
2022-05-21 17:49:54 +0900

    commit

--- test_power.py
+++ test_power.py
@@ -1,12 +1,27 @@
 from pptx import Presentation
 
 prs = Presentation()
-title_slide_layout = prs.slide_layouts[0]
+#title_slide_layout = prs.slide_layouts[0]
+title_slide_layout = prs.slide_layouts[1] 
+
+
 slide = prs.slides.add_slide(title_slide_layout)
+
+#追加：シートの追加
+slide2 = prs.slides.add_slide(title_slide_layout)
+
 title = slide.shapes.title
+
+#追加:
+title2 = slide2.shapes.title
+subtitle = slide2.placeholders[1] 
+
 subtitle = slide.placeholders[1]
 
 title.text = "Hello, World!"
-subtitle.text = "python-pptx was here!"
+title2.text = "Welcome"
 
-prs.save('test.pptx')
+subtitle.text = "python-pptx was here!"
+subtitle.text = "!World!"
+#prs.save('test.pptx')
+prs.save('test2.pptx')    
--- test_word.py
+++ test_word.py
@@ -5,4 +5,6 @@ document = Document()
 document.add_heading('簡単なWordドキュメントのタイトル', 0)
 document.add_paragraph('簡単なWordドキュメントのテキスト')
 
+document.add_picture('./photo.jpeg')
+
 document.save('sample.docx')
