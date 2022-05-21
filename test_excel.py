66f43e528ec40b979f9bd98dec9d007abe7398b6
yamamotoryuo <2004240023yr@edu.oca.ac.jp>
2022-05-21 17:00:10 +0900

    commit

--- test_excel.py
+++ test_excel.py
@@ -4,42 +4,33 @@ wb = Workbook()
 # grab the active worksheet
 ws = wb.active
 
-# Data can be assigned directly to cells
-ws['A1'] = 42
+ws1 = wb.create_sheet("Mysheet")
 
+ws1 = wb['Mysheet']
+
+cells = ws1['A1':'J10']
+
+i = 1
+
+for row in cells:
+    for cell in row:
+        cell.value = i
+        i += 1
+
+ws['A1'] = 42
 # Rows can also be appended
 ws.append([1, 2, 3])
-
 # Python types will automatically be converted
 import datetime
 ws['A2'] = datetime.datetime.now()
 
-#--追加
-
+# --追加
 #for x in range(1,101):
 #    for y in range(1,101):
 #        ws.cell(row=x,column=y)
-
-a = 'A'
-b = 'B'
-#for x in range(1,101):
-#    for y in range(1,101):
-#        d = a + str(x)
-#        D = b + str(x)
-#        ws[d] = x
-#        ws[D] = x
-
-for x in range(1,11):
-    for y in range(1,11):
-        ws.cell(row=x,column=y)
-
-
+#a ='A'
 #d = 10
 #c = a + str(d)
 #ws[c] = 10
-
-
-
-
 # Save the file
 wb.save("sample.xlsx")
